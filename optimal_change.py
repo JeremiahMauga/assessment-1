5aa2e96888cbed8a9589999b5fa45141ad63b03f
Jeremiah Mauga <miahmauga@gmail.com>
2021-06-04 14:20:50 -0700

    Jeremiahs assessment-1

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,73 @@
-# Write your solution here!
+import math
+
+def get_change(cost, paid):
+    # Create a map of money to run our change through
+    money = {"$100 dollar bill":100,"$50 dollar bill":50, "$20 dollar bill":20, "$10 dollar bill":10, "$5 dollar bill":5, "$1 dollar bill":1, "quarter":.25, "dime":.10, "penny":.01}
+    
+    # Create a variable to hold our initial subtraction (Round to the nearest 2nd decimal place!)
+    change = round((paid - cost), 2)
+
+    # Check to see if any change is returned
+    if change == 0:
+        return("Perfect Change!")
+    # Checking to see if the amount paid was enough to meet the cost
+    if change >= 0:
+        pass
+    else :
+        return("Not enough money!")
+    
+    # Lists to push our amounts
+    list_howMany = []
+    list_howMuch = []
+    
+    # Loop through our money map and assign out values to our lists
+    for key in money :
+        if money[key] <= change :
+                amount_of_bills = int(change // money[key])
+                change = round((change - money[key] * amount_of_bills), 2)
+                list_howMany.append(amount_of_bills)
+                list_howMuch.append(money[key])
+    
+    # Create an empty string that will hold our values in the correct format
+    input = " "
+    
+    # Loop through both of our lists and add them in the correct format to our list
+    for howMany, howMuch in zip(list_howMany, list_howMuch):
+        if howMuch >= 1: 
+            input += f"{howMany} ${howMuch}"
+            if howMany > 1:
+                input += " bills, "
+            else: 
+                input += " bill, "
+        elif howMuch == .25:
+            input += f"{howMany}"
+            if howMany > 1:
+                input += " quarters, "
+            else: 
+                input += " quarter, "
+        elif howMuch == .10:
+            input += f"{howMany}"
+            if howMany > 1:
+                input += " dimes, "
+            else: 
+                input += " dime, "
+        elif howMuch == .01:
+            input += f"and {howMany}"
+            if howMany > 1:
+                input += " pennies."
+            else: 
+                input += " penny."
+    
+    # Assign last variable to hold our final statement, taking in all appropriate variables
+    answer = (f"The optimal change for an item that costs ${cost} with an amount paid of ${paid} is{input}")
+    
+    print(answer.strip(' ,'))
+    return(answer.strip(' ,'))
+    
+# My attempt to factor in the "and" failed. Below are the items I was working with.
+# list_of_money = []
+# list_of_money.append(f"{howMany} ${howMuch}")
+# for x in list_of_money:
+    #     Add an "and" in front of the last item in the list
+    #     Append all items to a string
+# print(list_of_money)
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,26 @@
-# Write your unit tests here!
+import unittest
+
+from optimal_change import get_change
+
+class TestGetChange(unittest.TestCase) :
+      
+    def test_get_change_0(self) :
+        self.assertEqual(get_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
+    def test_get_change_1(self) :
+        self.assertEqual(get_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+    def test_get_change_2(self) :
+        self.assertEqual(get_change(1000, 100), "Not enough money!")
+
+    def test_get_change_3(self) :
+        self.assertEqual(get_change(1, 1), "Perfect Change!")
+
+    def test_get_change_4(self) :
+        self.assertEqual(get_change(49.99, 50), "The optimal change for an item that costs $49.99 with an amount paid of $50 is 1 penny.")
+
+    def test_get_change_1(self) :
+        self.assertEqual(get_change(256, 500), "The optimal change for an item that costs $256 with an amount paid of $500 is 2 $100 bills, 2 $20 bills, and 4 $1 bills")
+
+if __name__ == '__main__':
+    unittest.main()
